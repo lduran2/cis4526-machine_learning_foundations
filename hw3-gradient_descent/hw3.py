d5027c97b50763c5b071060a4e098cf4d01b248d
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2021-11-05 16:34:38 -0400

    now using numpy

--- hw3-gradient_descent/hw3.py
+++ hw3-gradient_descent/hw3.py
@@ -5,14 +5,17 @@ r'''
  differentiation.
 
  By        : Leomar Durán <https://github.com/lduran2/>
- When      : 2021-11-05t15:44
+ When      : 2021-11-05t16:31
  Where     : Temple University
  For       : CIS 4526
- Version   : 1.0.1
- Dataset   : https://archive.ics.uci.edu/ml/datasets/Wine
+ Version   : 1.0.2
+ Dataset   : https://archive.ics.uci.edu/ml/datasets/wine+quality
  Canonical : https://github.com/lduran2/cis-4526-hw2-kNN-vs-pocket/blob/master/hw2.py
 
  CHANGELOG :
+    v1.0.2 - 2021-11-05t16:31
+        now using numpy
+
     v1.0.1 - 2021-11-05t15:44
         named the features
 
@@ -21,40 +24,19 @@ r'''
  '''
 #
 
-from csv import DictReader
+import numpy as np
 
-DATA_FILENAME = r'wine.data'  # name of file holding the dataset
-
-FEATURES = (  # tuple of the names of features
-    r'Alcohol',
-    r'Malic_acid',
-    r'Ash',
-    r'Alcalinity_of_ash',
-    r'Magnesium',
-    r'Total_phenols',
-    r'Flavanoids',
-    r'Nonflavanoid_phenols',
-    r'Proanthocyanins',
-    r'Color_intensity',
-    r'Hue',
-    r'OD280__OD315_of_diluted_wines',
-    r'Proline'
-) # end FEATURES
+DATA_FILENAME = r'winequality-white.csv'  # name of file holding the dataset
+DELIMITER = ';' # used to separate values in DATA_FILENAME
 
 def main():
     r'''
      classify the wine samples
      '''
-    # open the CSV file
-    with open(DATA_FILENAME) as datafile:
-        # place a dictionary reader on the dataset
-        reader = DictReader(datafile, fieldnames=FEATURES)
-
-        # loop through the rows
-        for row in reader:
-            print(row)
-        # end for row in reader
-    # end with open(DATA_FILENAME) as csvfile
+    # read the dataset into a matrix
+    dataset = np.genfromtxt(DATA_FILENAME, delimiter=DELIMITER, skip_header=True, dtype=np.float64)
+    print(dataset.dtype.names)
+    print(dataset)
 # end def main()
 
 # run main if this package was run
