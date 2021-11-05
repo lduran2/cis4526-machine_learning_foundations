49e95b3b688e82177d98e049ad44e3278503e843
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2021-11-05 17:35:27 -0400

    extracted the labels (using matrix multiplication)

--- hw3-gradient_descent/hw3.py
+++ hw3-gradient_descent/hw3.py
@@ -5,14 +5,17 @@ r'''
  differentiation.
 
  By        : Leomar Durán <https://github.com/lduran2/>
- When      : 2021-11-05t16:31
+ When      : 2021-11-05t17:27
  Where     : Temple University
  For       : CIS 4526
- Version   : 1.0.2
+ Version   : 1.0.3
  Dataset   : https://archive.ics.uci.edu/ml/datasets/wine+quality
  Canonical : https://github.com/lduran2/cis-4526-hw2-kNN-vs-pocket/blob/master/hw2.py
 
  CHANGELOG :
+    v1.0.3 - 2021-11-05t17:27
+        extracted the labels (using matrix multiplication)
+
     v1.0.2 - 2021-11-05t16:31
         now using numpy
 
@@ -35,8 +38,25 @@ def main():
      '''
     # read the dataset into a matrix
     dataset = np.genfromtxt(DATA_FILENAME, delimiter=DELIMITER, skip_header=True, dtype=np.float64)
+    N_COLS = dataset.shape[1]
     print(dataset.dtype.names)
     print(dataset)
+
+    # get the labels
+    # function to classify the label scalars
+    classify = np.vectorize(lambda x : (+1) if (x in range(7,10)) else (-1))
+    # ZERO rows to remove features
+    label_mcand_features = np.zeros(((N_COLS - 1),))
+    # ONE row to keep the label
+    label_mcand_one = np.array([1])
+    # concatenate ZERO and ONE rows to a vector
+    label_mcand = np.concatenate(
+        (label_mcand_features, label_mcand_one), axis=0)
+    # multiply the dataset and labels
+    labels_scalars = (dataset @ label_mcand)
+    # and classify
+    labels = classify(labels_scalars)
+    print(labels)
 # end def main()
 
 # run main if this package was run
