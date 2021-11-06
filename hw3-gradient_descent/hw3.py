decce2dec3403d707d6eec782716484c9c2f1155
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2021-11-05 21:04:09 -0400

    abstracted the parts of the main method ater data split

--- hw3-gradient_descent/hw3.py
+++ hw3-gradient_descent/hw3.py
@@ -5,17 +5,19 @@ r'''
  differentiation.
 
  By        : Leomar Durán <https://github.com/lduran2/>
- When      : 2021-11-05t19:38
+ When      : 2021-11-05t21:03
  Where     : Temple University
  For       : CIS 4526
- Version   : 1.1.0
+ Version   : 1.1.1
  Dataset   : https://archive.ics.uci.edu/ml/datasets/wine+quality
  Canonical : https://github.com/lduran2/cis4526-machine_learning_foundations/blob/master/hw3-gradient_descent/hw3.py
 
  CHANGELOG :
+    v1.1.1 - 2021-11-05t21:03
+        abstracted the parts of the main method
+
     v1.1.0 - 2021-11-05t19:38
         split train, test data, calculated dimensionality
-        
 
     v1.0.5 - 2021-11-05t18:56
         removing invalid rows
@@ -53,9 +55,22 @@ def main():
     dataset = np.genfromtxt(
         DATA_FILENAME, delimiter=DELIMITER,
         skip_header=True, dtype=np.float64)
+    # get the training and testing data
+    (features, labels) = splitFeaturesLabels(dataset)
+    (train_x, test_x, train_y, test_y, \
+            num_test, num_train, num_dims) = \
+        splitTrainTest(features, labels)
+    #
+# end def main()
+
+def splitFeaturesLabels(dataset):
+    r'''
+     Divides the dataset into features and labels.
+     @param dataset : 'numpy.ndarray' = the dataset to divide
+     @return a tuple containing the features and the labels
+     '''
+    # get the number of rows and columns
     (num_rows, num_cols) = dataset.shape
-    print(dataset.dtype.names)
-    print(dataset)
 
     # divide into features and labels
     # function to classify the label scalars
@@ -67,30 +82,38 @@ def main():
     v_label_scalars = M_label_scalars.reshape((num_rows,))
     # classify the labels
     unvalid_labels = vec_classify(v_label_scalars)
+
     # remove all 0 rows
     # 0 rows are when the label scalar is neither in [7..10[ nor [3..6[
     should_keep_rows = (unvalid_labels != 0)
-    print(should_keep_rows)
     features = unvalid_features[should_keep_rows,:]
     labels = unvalid_labels[should_keep_rows]
-    print(features)
-    print(labels)
 
+    return (features, labels)
+# end def splitFeaturesLabels(dataset)
+
+def splitTrainTest(features, labels):
+    r'''
+     Divides the train, test data × features, labels.
+     @param features : 'numpy.ndarray' = of the dataset
+     @param labels : 'numpy.ndarray' = of the dataset
+     @return a tuple containing the (train_x, test_x, train_y, test_y),
+        the number of testing and training examples, and the
+        dimensionality
+     '''
     (num_examples,) = labels.shape          # num of valid examples
     num_test = (num_examples//4)            # num of test examples
     num_train = (num_examples - num_test)   # num of training examples
     num_dims = features.shape[1]            # dimensionality
-    print(r'train:', num_train, r'; test:', num_test)
 
     # split features
     (train_x, test_x) = np.split(features, (num_train,), axis=0)
     # split labels
     (train_y, test_y) = np.split(labels, (num_train,), axis=0)
-    print(train_x)
-    print(test_x)
-    print(train_y)
-    print(test_y)
-# end def main()
+
+    return (train_x, test_x, train_y, test_y, \
+        num_test, num_train, num_dims)
+# end def splitTrainTest(features, labels)
 
 def classify(scalar):
     r'''
@@ -110,7 +133,7 @@ def classify(scalar):
         result = -1
     # end elif (scalar in NEGATIVE_RANGE)
     return result
-# end
+# end def classify(scalar)
 
 # run main if this package was run
 if (__name__==r'__main__'):
